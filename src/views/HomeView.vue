1d9bac770c5d0a5599cb2f6036605e0a0a478dff
jeffspatafora <jeffspatafora@gmail.com>
2022-04-20 13:33:34 -0700

    adds index action to create

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,50 @@
+<script>import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome!",
+      cars: [],
+      newCarParams: {}
+    };
+  },
+  created: function () {
+    this.carsIndex()
+  },
+  methods: {
+    carsIndex: function () {
+      console.log("index");
+      axios.get("/cars.json").then(response => {
+        console.log(response.data);
+        this.cars = response.data;
+      })
+    }
+  },
+  carCreate: function () {
+
+  }
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div>
+      <!-- <p>Name: <input type="text" v-model="" /></p> -->
+    </div>
+    <div v-for="car in cars" v-bind:key="car.id">
+      <h2>{{ car.make }} {{ car.model }}</h2>
+      <!-- <p>{{ car.model }}</p> -->
+      <p>{{ car.year }}</p>
+      <p>{{ car.color }}</p>
+      <img v-bind:src="car.image_url" />
+      <hr>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+<style>
+img {
+  width: 200px;
 }
-</script>
+</style>
